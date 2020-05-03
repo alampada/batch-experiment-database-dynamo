3c7d6da4f02c620700fa6e9e0ba3c63db58902ce
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2020-05-03 13:44:39 +0100

    Fix h2 job repository

--- src/main/java/com/example/batchdatabase/configuration/BatchConfiguration.java
+++ src/main/java/com/example/batchdatabase/configuration/BatchConfiguration.java
@@ -1,12 +1,18 @@
 package com.example.batchdatabase.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.batch.BatchDataSourceInitializer;
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
@@ -14,17 +20,18 @@ import javax.sql.DataSource;
  * So that we use a different db for the source of data and a different database for spring batch metadata.
  * https://stackoverflow.com/questions/25256487/how-to-java-configure-separate-datasources-for-spring-batch-data-and-business-da
  */
-@Configuration
+@Component
 public class BatchConfiguration extends DefaultBatchConfigurer {
 
     @Override
-    public void setDataSource(@Qualifier("metadataDataSource") DataSource batchDataSource) {
-        super.setDataSource(batchDataSource);
+    @Autowired
+    public void setDataSource(@Qualifier("metadataDataSource") DataSource dataSource) {
+        super.setDataSource(dataSource);
     }
 
     @Bean
-    public BatchDataSourceInitializer batchDataSourceInitializer(@Qualifier("metadataDataSource") DataSource batchDataSource,
-                                                                 ResourceLoader resourceLoader) {
-        return new BatchDataSourceInitializer(batchDataSource, resourceLoader, new BatchProperties());
+    public BatchDataSourceInitializer  batchDataSourceInitializer(@Qualifier("metadataDataSource") DataSource dataSource, ResourceLoader resourceLoader){
+        BatchDataSourceInitializer batchDatabaseInitializer = new  BatchDataSourceInitializer(dataSource, resourceLoader, new BatchProperties());
+        return batchDatabaseInitializer;
     }
 }
