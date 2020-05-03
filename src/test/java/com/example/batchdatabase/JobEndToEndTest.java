dd51e351e1582a067055f26c359fc91f1eb5f21c
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2020-05-03 17:48:28 +0100

    Optimize imports

--- src/test/java/com/example/batchdatabase/JobEndToEndTest.java
+++ src/test/java/com/example/batchdatabase/JobEndToEndTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 import static org.mockito.ArgumentMatchers.any;
 
 @SpringBatchTest
